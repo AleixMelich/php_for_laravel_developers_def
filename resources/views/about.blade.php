97b34dbb863c0fb78226b405c6922dcbbffb9e10
«AleixMelich» <aleixmelich@iesebre.com>
2023-02-19 19:55:14 +0100

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <div class="min-h-full">
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+                    About
+                </h1>
+            </div>
+        </header>
+        <main>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eligendi numquam ratione voluptatibus. Adipisci aspernatur, delectus dolor dolores facere laboriosam mollitia nesciunt nihil optio possimus quis ratione repudiandae sapiente voluptate?
+        </main>
+    </div>
+</x-layout>
